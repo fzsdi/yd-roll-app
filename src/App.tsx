a6dbf46c87dea43f3f1f23e84568fd01be1510f0
fzsdi <saeedifaezeh@gmail.com>
2022-04-18 18:12:07 +0430

    deleted websocket test

--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,6 @@ import Content from './Content';
 import LoginPage from './LoginPage';
 
 function App() {
-  var testSocket = new WebSocket("ws://localhost:443/ws");
-  testSocket.onmessage = function (e) {
-    alert(e.data);
-  }
-
-  function Send() {
-    testSocket.send("texttext");
-  }
-  Send();
   const myStorage = window.sessionStorage;
   const userToken = myStorage.getItem('Token');
 
