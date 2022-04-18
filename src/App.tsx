d07f4294ed85a4e0856a709525f9cdd584fd8653
fzsdi <saeedifaezeh@gmail.com>
2022-04-18 18:11:18 +0430

    modified base url

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,15 @@ import Content from './Content';
 import LoginPage from './LoginPage';
 
 function App() {
-  // const [isLoggedIn, setIsLoggedIn] = useState(false);
+  var testSocket = new WebSocket("ws://localhost:443/ws");
+  testSocket.onmessage = function (e) {
+    alert(e.data);
+  }
+
+  function Send() {
+    testSocket.send("texttext");
+  }
+  Send();
   const myStorage = window.sessionStorage;
   const userToken = myStorage.getItem('Token');
 
