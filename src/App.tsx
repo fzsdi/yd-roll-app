5188d9f82b0556f50683fe1512058fab999a7eaa
fzsdi <saeedifaezeh@gmail.com>
2022-04-24 00:02:56 +0430

    added websocket connection

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
+import { Component } from 'react';
 import './App.css';
 import Content from './Content';
 import LoginPage from './LoginPage';
 
-function App() {
-  const myStorage = window.sessionStorage;
-  const userToken = myStorage.getItem('Token');
+const myStorage = window.sessionStorage;
+const userToken = myStorage.getItem('Token');
+let socket = new WebSocket(`ws://localhost:8080/channel?token=${userToken}`);
 
-  return userToken ? <Content /> : <LoginPage />
+export default class App extends Component {
+  
+  componentDidMount() {
+    socket.onopen = () => {
+      alert("[WebSocket] connection established.");
+    };
+  }
+  
+  render() {
+    return userToken ? <Content /> : <LoginPage />
+  }
 }
-
-export default App;
